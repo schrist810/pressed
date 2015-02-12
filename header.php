0f4cf4536e9da0965a22235318fd3d11302e4040
Sarah Christ <schrist810@gmail.com>
2015-02-11 18:54:29 -0600

    Header and Footer

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' );?>; charset=<?php bloginfo( 'charset' ); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<?php wp_head(); ?>
+	<title><?php bloginfo('name'); ?></title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+</head>
+	<body> <?php body_class( $class ); ?>>
+		<header>
+			<h1><?php bloginfo('name'); ?></h1>
+			<p>A fresh new WordPress site delivered!</p>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
